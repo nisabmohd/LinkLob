13e5dbeb53148f4a514a344bf5dae0309563ceb9
nisabmohd <nisabmohd@gmail.com>
2024-04-17 18:31:44 +0530

    init

---
+++ app/[id]/page.tsx
@@ -0,0 +1,58 @@
+export default function page() {
+  return (
+    <div className="flex justify-center min-h-screen py-6 sm:py-12">
+      <div className="flex flex-col gap-12 max-w-3xl w-full px-4 md:px-10">
+        <div>
+          <div className="flex items-start justify-between">
+            <div className="flex flex-col items-start gap-2">
+              <h1 className="text-3xl font-extrabold tracking-tight">
+                Taxing Laughter: The Joke Tax Chronicles
+              </h1>
+              <p className="leading-none text-muted-foreground">
+                Posted 2 hours ago
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="leading-7 prose dark:prose-invert max-w-full">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+              libero officia quod quas necessitatibus, consectetur similique eum
+              dolore asperiores aperiam cum aliquid hic corporis neque delectus,
+              labore dolorum? Rerum natus odio consequatur ipsa exercitationem
+              soluta magnam quibusdam. Dignissimos aperiam veniam nemo
+              laudantium! Odio neque ad sed nobis suscipit id aspernatur nam
+              quas amet laudantium fuga corrupti qui tenetur maxime velit
+              laborum quasi incidunt eaque itaque dignissimos, illum ipsum
+              consequuntur maiores? Consequuntur ab delectus eaque ex quo
+              officia modi! Natus ee voluptatem sequi explicabo! Ullam similique
+              cum facere dicta incidunt libero ex vm corrupti perferendis
+              temporibus aliquid harum sunt reprehenderit ea. Numquam vero
+              assumenda recusandae laboriosam dolores quia odit nihil
+              voluptatibus consequatur quos eligendi sit, eius non quis modi
+              sint veniam impedit quam earum architecto aut asperiores commodi
+              fuga? Sequi ad consequuntur porro, voluptas optio quia soluta
+              aspernatur itaque. Labore iusto enim dolor ad minima saputem
+              asperiores dolorem? Consectetur adipisci aliquid et, facere
+              impedit molestias nesciunt illo velit maiores magni dolor quas
+              iure ab autem dicta reprehenderit minus quasi facilis veniam
+              dolore, optio nobis, sapiente nostrum nemo! Atque dolorum odio,
+              dignissimos nulla sit sequi natus reprehenderit ullam!
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptatem, incidunt natus illum in dolor commodi iste dignissimos
+              ipsam dolorum! Consectetur mollitia repellat quaerat eligendi
+              natus minus nihil? Magnam, accusantium possimus! Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Vero accusantium corrupti,
+              illo voluptatem qui veritatis fugit sapiente eligendi beatae
+              excepturi. Hic magnam maxime esse suscipit? Nam eligendi
+              consectetur dignissimos temporibus!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
